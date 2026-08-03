e7e6d55697b4c9568f1bc2182977c1e329c0ddf5
qwertygamer720 <qwertygamer720@gmail.com>
2026-08-03 14:50:43 -0400

    new folder for material
    player movement is not workings

--- Final/Assets/Scripts/PlayerMovement.cs
+++ Final/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb;
-    float horizontalInput;
+    Animator anim;
+    Rigidbody2D rb2d;
+    float horizontalInput ;
+    //player movement
     public float moveSpeed = 10f;
     public float jumpSpeed = 5f;
+     //variables for cheking if player is grounded
+    public Transform GroundCheckPoint;
+
+    public LayerMask GroundLayer;
+    float groundCheckRadius = 0.2f;
+    //reset player position
+    public float gameOverHeight = -4f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +31,36 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         float nextVelocityX = horizontalInput * moveSpeed;
-        float nextVelocityY = rb.linearVelocity;
+        float nextVelocityY = rb2d.linearVelocity.y;
+        bool isGrounded = CheckGrounded();
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            nextVelocityY = jumpSpeed;
+        }
+        rb2d.linearVelocity = new Vector2(nextVelocityX, nextVelocityY);
+        //change player orientation based on input
+        if(horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+           
+        }
+         else if(horizontalInput > 0)
+        {
+             transform.localScale = new Vector3(1,1,1);   
+        }
+        //animation parameters
+        anim.SetFloat("Xspeed", Mathf.Abs(nextVelocityX));
+        anim.SetFloat("Yspeed", nextVelocityY);
+        anim.SetBool("Grounded", isGrounded);
+        //Restrat if below certian height
+        if(transform.position.y < gameOverHeight)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    bool CheckGrounded()
+    {
+        return Physics2D.OverlapCircle(GroundCheckPoint.position, groundCheckRadius, GroundLayer);
+
     }
 }
