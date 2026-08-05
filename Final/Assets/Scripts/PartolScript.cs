7164162ca2612275df7f97bcbfb47917ab1885a8
qwertygamer720 <qwertygamer720@gmail.com>
2026-08-05 13:02:57 -0400

    updated patrol script a bit

--- Final/Assets/Scripts/PartolScript.cs
+++ Final/Assets/Scripts/PartolScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PartolScript : MonoBehaviour
 {
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+
+    public float speed;
+
+//add animator later
+
+    private Transform currentPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        //add something for getting the animator component
+        currentPoint = pointB.transform;
+        // animation bool goes here
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 point = currentPoint.position - transform.position;
+        if ( currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
     }
 }
