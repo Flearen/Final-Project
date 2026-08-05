23c04a48be98253b37f98ed5acfea4f93751a8e9
aidanharter64-ai <aidanharter64@gmail.com>
2026-08-05 10:05:07 -0400

    Changed dash to q

--- Final/Assets/Scripts/higher jump + dash.cs
+++ Final/Assets/Scripts/higher jump + dash.cs
@@ -38,7 +38,7 @@ public class dash : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        if (Input.GetKeyDown(KeyCode.Q) && canDash)
         {
             StartCoroutine(Dash());
         }
