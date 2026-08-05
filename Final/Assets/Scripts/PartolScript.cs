67e96a8c11786c59981a46ea1634b010b2bbc2df
qwertygamer720 <qwertygamer720@gmail.com>
2026-08-05 13:36:29 -0400

    patrol script done(maye remove later)

--- Final/Assets/Scripts/PartolScript.cs
+++ Final/Assets/Scripts/PartolScript.cs
@@ -17,6 +17,7 @@ public class PartolScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        flip();
         rb = GetComponent<Rigidbody2D>();
         //add something for getting the animator component
         currentPoint = pointB.transform;
@@ -29,11 +30,28 @@ public class PartolScript : MonoBehaviour
         Vector2 point = currentPoint.position - transform.position;
         if ( currentPoint == pointB.transform)
         {
-            rb.velocity = new Vector2(speed, 0);
+            rb.velocity = new Vector2(speed , 0);
         }
         else
         {
-            rb.velocity = new Vector2(-speed, 0);
+            rb.velocity = new Vector2(-speed , 0);
         }
+
+        if (Vector2.Distance(transform.position, currentPoint.position)< 0.5f && currentPoint == pointB.transform)
+        {
+            flip();
+            currentPoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position)< 0.5f && currentPoint == pointA.transform)
+        {
+            flip();
+            currentPoint = pointB.transform;
+        }
+    }
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
 }
