d81ee3f0ebe5c0b3905eaafaa82bbcab98bee41b
qwertygamer720 <qwertygamer720@gmail.com>
2026-08-05 20:40:58 -0400

    Urchin has radius to where it can see player

--- Final/Assets/Scripts/enemyShooting.cs
+++ Final/Assets/Scripts/enemyShooting.cs
@@ -7,23 +7,32 @@ public class enemyShooting : MonoBehaviour
     public GameObject bullet;
     public Transform bulletPos;
 
+    private GameObject player;
     private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        
 
-        if(timer > 2)
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        
+        if (distance <= 10)
         {
-            timer = 0;
-            shoot();
+            timer += Time.deltaTime;
+
+            if(timer > 2)
+            {
+              timer = 0;
+              shoot();
+            }   
         }
+        
     }
     void shoot()
     {
