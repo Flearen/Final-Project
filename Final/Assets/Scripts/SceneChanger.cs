3958fd6776f75c7c19d33dd121b53140d38423de
Flearen <pinkskyandclouds@gmail.com>
2026-08-07 13:23:48 -0400

    restructure

--- Final/Assets/Scripts/SceneChanger.cs
+++ Final/Assets/Scripts/SceneChanger.cs
@@ -6,12 +6,24 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     public string sceneToLoad;
+    public Animator fadeAnim;
+    public float fadeTime = .5f;
 
     private void OnTriggerEnter2D(Collider2D collision){
 
         if (collision.gameObject.tag == "Player"){
 
             SceneManager.LoadScene(sceneToLoad);
+            fadeAnim.Play("fadeout");
+            StartCoroutine(DelayFade());
         }
+           
+
+        }       
+    
+    IEnumerator DelayFade(){
+
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
--- Final/Assets/Scripts/activate.cs
+++ Final/Assets/Scripts/activate.cs
@@ -13,15 +13,4 @@ public class activate : MonoBehaviour
     }
 
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
